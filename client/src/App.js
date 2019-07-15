3a36e301b5e7d5f2adeb878d2276cb9e48ed33d3
Seohyun Yoon <sarahseohyun@gmail.com>
2019-07-15 20:10:44 +0900

    client getToDo() 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: {}
+    }
+  }
+
+  getToDo = async () => {
+    try {
+      const response = await axios.get('/todo');
+      this.setState({
+        todo: response.data
+      })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Hello Stranger?</h1>
+        <button onClick={this.getToDo}>
+          TODO
+        </button>
+        <h3>name: {this.state.todo.name}</h3>
+        <h4>description: {this.state.todo.description}</h4>
       </div>
     )
   }
+
 }
 
 export default App;
