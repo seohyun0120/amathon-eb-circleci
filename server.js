01e7a084a1d77f8f60ebf1e6d1f7f5dd368e1686
Seohyun Yoon <sarahseohyun@gmail.com>
2019-07-15 15:42:55 +0900

    Initial Setting

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const PORT = process.env.HTTP_PORT || 4001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}.`);
+});
