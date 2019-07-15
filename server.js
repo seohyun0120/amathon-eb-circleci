c48914179d63a2a41837a6f6da0c5d6dd0c747bb
Seohyun Yoon <sarahseohyun@gmail.com>
2019-07-15 20:11:05 +0900

    server /todo 추가

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const PORT = process.env.HTTP_PORT || 4001;
 
 const app = express();
 
+app.get('/todo', (req, res) => {
+  res.json(
+    {
+      name: 'CircleCI',
+      description: 'setting CI/CD with CircleCI'
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
 })
