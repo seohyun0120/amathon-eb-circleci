901b2f33cb445d5323fda1f1e71fce670348f60a
Seohyun Yoon <sarahseohyun@gmail.com>
2019-07-25 01:50:51 +0900

    EB proxy setting

---
+++ client/build/precache-manifest.68ff129c0eb8060946ac21f2fcf93ec5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e56d3a5b66f35f6ab9f618310797ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f31acbc485e27e59bed",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "4a062055031b3ef3c752",
+    "url": "/static/js/2.a3816b16.chunk.js"
+  },
+  {
+    "revision": "1f31acbc485e27e59bed",
+    "url": "/static/js/main.e605d573.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
