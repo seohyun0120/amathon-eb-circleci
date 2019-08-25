cd9cebea2c8304087e5801647000ec1e3b9de095
Seohyun Yoon <sarahseohyun@gmail.com>
2019-08-26 01:19:36 +0900

    refactoring

--- server.compiled.js
+++ server.compiled.js
@@ -1,19 +1,23 @@
 "use strict";
 
-var _path = _interopRequireDefault(require("path"));
+var path = require('path');
 
-var _express = _interopRequireDefault(require("express"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var express = require('express');
 
 var PORT = process.env.HTTP_PORT || 4001;
-var app = (0, _express["default"])();
-app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
-app.get('/todo', function (req, res) {
-  res.json({
-    name: 'CircleCI',
-    description: 'setting CI/CD with CircleCI'
-  });
+var app = express();
+var todoList = [{
+  id: 0,
+  title: 'Elastic Beanstalk',
+  description: 'ElasticBeanstalk으로 배포하기'
+}, {
+  id: 1,
+  title: 'CircleCI',
+  description: 'CircleCI로 CI/CD 세팅하기'
+}];
+app.use(express["static"](path.join(__dirname, 'client', 'build')));
+app.get('/todos', function (req, res) {
+  res.json(todoList);
 });
 app.listen(PORT, function () {
   console.log("Server listening at port ".concat(PORT, "."));
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-import path from 'path';
-import express from 'express';
-
+const path = require('path');
+const express = require('express');
 const PORT = process.env.HTTP_PORT || 4001;
+
 const app = express();
+
+const todoList = [
+  {
+    id: 0,
+    title: 'Elastic Beanstalk',
+    description: 'ElasticBeanstalk으로 배포하기'
+  },
+  {
+    id: 1,
+    title: 'CircleCI',
+    description: 'CircleCI로 CI/CD 세팅하기'
+  }
+]
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('/todo', (req, res) => {
-  res.json(
-    {
-      name: 'CircleCI',
-      description: 'setting CI/CD with CircleCI'
-    });
+app.get('/todos', (req, res) => {
+  res.json(todoList);
 });
 
 app.listen(PORT, () => {
